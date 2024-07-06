45e7005974b52a0046ee3956f55546d3c8bb510b
tharindu907 <tsandamalamarathunga@gmail.com>
2024-07-06 20:48:04 +0530

    dashboard added to all other pages

--- src/Pages/AccountantPage.jsx
+++ src/Pages/AccountantPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import NavigationBar from '../Components/NavigationBar/NavigationBar';
+import Dashboard from '../Components/Dashboard/Dashboard';
 
 function AccountantPage() {
   return (
     <div>
       <h1>Accountant Dashboard</h1>
+      <NavigationBar />
+      <Dashboard />
     </div>
   );
 }
--- src/Pages/StudentPage.jsx
+++ src/Pages/StudentPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import NavigationBar from '../Components/NavigationBar/NavigationBar';
+import Dashboard from '../Components/Dashboard/Dashboard';
 
 function StudentPage() {
   return (
     <div>
       <h1>Student Dashboard</h1>
+      <NavigationBar/>
+      <Dashboard />
     </div>
   );
 }
