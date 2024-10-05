73259be238af7f87978f63a3782c423987f4b6ee
tharindu907 <tsandamalamarathunga@gmail.com>
2024-10-06 00:42:05 +0530

    Responsive Admin Page is created

--- src/Components/NavigationBar/NavigationBar.jsx
+++ src/Components/NavigationBar/NavigationBar.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './NavigationBar.css';
 import { useUserContext } from '../../Context/UserContext';
-import defaultProfilePic from '../Assets/default-profile-pic.png';
+import defaultProfilePic from '../Assets/avatar.svg';
 
 const NavigationBar = () => {
-  const { isLoggedIn, userType, username, profilePic } = useUserContext();
+  const { isLoggedIn, userType, username} = useUserContext();
 
   return (
     <div className="navigation-bar">
@@ -13,7 +13,7 @@ const NavigationBar = () => {
       </div>
       {isLoggedIn && (
         <div className="right-corner">
-          <img src={profilePic || defaultProfilePic} alt="Profile" className="profile-pic" />
+          <img src={defaultProfilePic} alt="Profile" className="profile-pic" />
           <span className="username">{username || 'User'}</span>
         </div>
       )}
