c83f22276d994e5969db820f71e0e2cfabc66b55
tharindu907 <tsandamalamarathunga@gmail.com>
2024-10-22 09:51:57 +0530

    ClassList component is created.

---
+++ src/Components/ClassList/ClassList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ClassList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ClassList
--- src/Pages/AdminClassesPage.jsx
+++ src/Pages/AdminClassesPage.jsx
@@ -6,11 +6,13 @@ import registrationIcon from '../Components/Assets/registration-icon.png';
 import detailsIcon from '../Components/Assets/details-icon.png';
 import paymentsIcon from '../Components/Assets/payments-icon.png';
 import timetableIcon from '../Components/Assets/timetable-icon.png';
+import listIcon from '../Components/Assets/listIcon.png'
 import TimeTable from '../Components/TimeTable/TimeTable'; // Import the TimeTable component
 import './AdminClassesPage.css';
 import ClassRegistration from '../Components/ClassRegistration/ClassRegistration';
 import ClassDetails from '../Components/ClassDetails/ClassDetails';
 import ClassPayments from '../Components/ClassPayments/ClassPayments';
+import ClassList from '../Components/ClassList/ClassList';
 
 const AdminClassesPage = () => {
   return (
@@ -41,6 +43,10 @@ const AdminClassesPage = () => {
                     <img src={timetableIcon} alt="Timetable" />
                     <div className="c-card-title">Timetable</div>
                   </Link>
+                  <Link to="list" className="classes-management-card">
+                    <img src={listIcon} alt="List" />
+                    <div className="c-card-title">List</div>
+                  </Link>
                 </div>
               </>
             }
@@ -48,7 +54,8 @@ const AdminClassesPage = () => {
           <Route path="registration" element={<ClassRegistration />} />
           <Route path="details" element={<ClassDetails />} />
           <Route path="payments" element={<ClassPayments />} /> 
-          <Route path="timetable" element={<TimeTable />} /> 
+          <Route path="timetable" element={<TimeTable />} />
+          <Route path="list" element={<ClassList />} /> 
         </Routes>
       </div>
     </div>
